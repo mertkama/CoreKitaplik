0700365fed2003f9be0785172715df7ddcab7e11
Mert <mert172317@gmail.com>
2022-03-01 17:03:46 +0300

    mvc projesi devam

---
+++ CoreKitaplik/ViewComponents/YeniKitaplar.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CoreKitaplik.Models;
+
+namespace CoreKitaplik.ViewComponents
+{
+    public class YeniKitaplar:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var kitaplistesi = new List<Kitaplar>
+            {
+                new Kitaplar
+                {
+                    ID=6,
+                    KitapAd="Korku",
+                    Yazar="Zweig"
+                },
+                new Kitaplar
+                {
+                    ID=7,
+                    KitapAd="Anna Karanina",
+                    Yazar="Tolstoy"
+                }
+            };
+            return View(kitaplistesi);
+        }
+    }
+}
